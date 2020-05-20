41abec09b76f3424d49ea4794f5178b653a7a29d
atiqulhb <atiqulhbhuiyan@github.com>
2020-05-20 15:44:52 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <h1>Coming Soon</h1>
+
+    <style jsx>{`
+      div {
+        width: 100%;
+        height: 100vh;
+        display: -webkit-flex;display: -moz-flex;display: -ms-flex;display: -o-flex;display: flex;
+        justify-content: center;
+        -ms-align-items: center;align-items: center;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
